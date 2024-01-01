9ecf52816f85c7b7b87d0505418964305a7bc881
aadriance <primaryartemis@outlook.com>
2023-12-31 22:05:03 -0800

    doc strings on functions

--- briefcase.go
+++ briefcase.go
@@ -31,15 +31,9 @@ var commands = []Command{
 	{info, "info", "Show information about the temp directory used by briefcase", "briefcase info"},
 	{set, "set", "Set a briefcase variable", "briefcase set <variable> <value>"},
 	{get, "get", "Get a briefcase variable", "briefcase get <variable>"},
-	{purge, "purge", "Purge briefcase data", "briefcase purge"},
+	{purge, "purge", "Purge briefcase data. Optionally allows [force] param to prevent prompting.", "briefcase purge [force]"},
 	{remove, "remove", "Remove a briefcase variable", "briefcase remove <variable>"},
 	{list, "list", "List briefcase entries", "briefcase list"},
-}
-
-var envVars = []string{
-	"BRIEFCASE_DIR",
-	"TEMP",
-	"TMPDIR",
 }
 
 func main() {
@@ -70,7 +64,16 @@ func main() {
 
 // utility functions
 
+// getTempDir returns the base directory used by the briefcase program to store data.
+// This is intended to be a 'temp' directory, but could be anything. This function
+// avoids using os.TempDir to allow reporting what environment variable was used.
 func getTempDir() TempDir {
+	var envVars = []string{
+		"BRIEFCASE_DIR",
+		"TEMP",
+		"TMPDIR",
+	}
+
 	for _, envVar := range envVars {
 		if dir := os.Getenv(envVar); dir != "" {
 			return TempDir{dir, envVar}
@@ -80,6 +83,8 @@ func getTempDir() TempDir {
 	return TempDir{"/tmp", "N/A"}
 }
 
+// getBriefcaseDirName determines the directory that will be created and used insited
+// the temp dir for actual data storage. briefcase by default.
 func getBriefcaseDirName() string {
 	if name := os.Getenv("BRIEFCASE_DIRNAME"); name != "" {
 		return name
@@ -88,22 +93,28 @@ func getBriefcaseDirName() string {
 	return "briefcase"
 }
 
+// printCommandInfo prints out the name, description, and usage of the command for
+// CLI help info.
 func printCommandInfo(cmd Command) {
 	fmt.Println(cmd.name)
 	fmt.Println("\tDescription: " + cmd.description)
 	fmt.Println("\tUsage: " + cmd.usage)
 }
 
+// getBriefcaseDir is a helper function to get the fully computed result of where
+// to store briefcase data based on the environment variables.
 func getBriefcaseDir() string {
 	return path.Join(getTempDir().path, getBriefcaseDirName())
 }
 
 // Commands to be invoked by the main program
 
+// version prints the version of briefcase
 func version(_ UserArgs) {
 	fmt.Println("Briefcase " + VERSION)
 }
 
+// help printa all commands briefcase has.
 func help() {
 	fmt.Println("")
 	for _, cmd := range commands {
@@ -111,6 +122,7 @@ func help() {
 	}
 }
 
+// info help the user get information about the temp dir used by briefcase.
 func info(_ UserArgs) {
 	tempInfo := getTempDir()
 	dirName := getBriefcaseDirName()
@@ -119,6 +131,7 @@ func info(_ UserArgs) {
 	fmt.Println("\tBriefcase Directory Name: " + dirName)
 }
 
+// set will store user provided data into the briefcase directory.
 func set(args UserArgs) {
 	briefcase := getBriefcaseDir()
 	if args.name == "" || args.value == "" {
@@ -138,6 +151,7 @@ func set(args UserArgs) {
 	}
 }
 
+// get retrieves data from the briefcase directory.
 func get(args UserArgs) {
 	briefcase := getBriefcaseDir()
 	if args.name == "" {
@@ -154,6 +168,8 @@ func get(args UserArgs) {
 	os.Stdout.Write(data)
 }
 
+// purge removes all briefcase data.
+// prompts user for confirmation if force is not provided.
 func purge(args UserArgs) {
 	var confirm string
 	if args.name == "force" {
@@ -171,6 +187,7 @@ func purge(args UserArgs) {
 	}
 }
 
+// remove deletes the data for the given breifcase entry.
 func remove(args UserArgs) {
 	briefcase := getBriefcaseDir()
 	if args.name == "" {
@@ -184,6 +201,7 @@ func remove(args UserArgs) {
 	}
 }
 
+// list dumps the full list of briefcase entries.
 func list(_ UserArgs) {
 	briefcase := getBriefcaseDir()
 	files, err := os.ReadDir(briefcase)
